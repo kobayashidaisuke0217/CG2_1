0d54b383d112f5d603ee3b364ceb3e893134a244
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-05-17 22:44:48 +0900

    コメントアウトを消した

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-//#include <Windows.h>
-//#include <cstdint>
-//#include <string>
-//#include "WinApp.h"
 #include"base/DirectXCommon.h"
 
 
@@ -12,7 +8,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	
 	WinApp* win_ = nullptr;
 	direct->Initialize(win_,1280,720);
-	//win_->CreateGameWindow(L"CG2",1280,720);
+	
 	while (true) {
 		direct->PreDraw();
 		// メッセージ処理
@@ -21,7 +17,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 		
 	direct->PostDraw();
-		//direct->ClearRenderTarget();
+		
 	}
 	direct->Finalize();
 	return 0;
