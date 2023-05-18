9307241606d9d4e534c8839d7185bc1b87966b7d
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-05-18 21:14:01 +0900

    エラー探し

--- base/MyEngine.cpp
+++ base/MyEngine.cpp
@@ -276,13 +276,13 @@ void MyEngine::Finalize()
 
 	/*CloseWindow(WinApp::GetHwnd());*/
 	//リソースリークチェック
-	/*IDXGIDebug1* debug;
+	IDXGIDebug1* debug;
 	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
-		debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);*/
-//		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
-	//	debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
-	//	debug->Release();
-	//}
+		debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+		debug->Release();
+	}
 }
 WinApp*MyEngine:: win_;
 DirectXCommon* MyEngine::direct_;
